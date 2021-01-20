a0eef5cbbb2c1cff7d672c8c78dcc26ff1a9160c
Felippe Nacif <felippenacif@gmail.com>
2021-01-20 14:59:48 -0300

    Melhorias na usabilidade. Memoria ao sair da extensao e clicar novamente

--- popup.js
+++ popup.js
@@ -1,23 +1,76 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    console.log(verifyIfHasActiveTimerAndShowCorrespondent)
+
     var sleepTabButton = document.getElementById('sleepTab');
     sleepTabButton.addEventListener('click', function () {
-        let min = +document.getElementById('minutesInput').value;
+        let min = +(document.getElementById('minutesInput').value);
         let alarmName;
         chrome.tabs.getSelected(null, function (tab) {
-            alarmName = tab.id + "-sleep-" + min;
-            chrome.alarms.create(this.alarmName, { delayInMinutes: min });
+            let tabId = tab.id
+            alarmName = tabId + "-sleep-" + min;
+            chrome.alarms.create(alarmName, { delayInMinutes: min });
+
+            var obj = {}
+            obj[tabId + ''] = {
+                expiresIn: +(new Date(new Date().setMinutes(new Date().getMinutes() + min))), // Somar minutos para data atual
+                alarmName: alarmName
+            }
+            chrome.storage.local.set(obj);
+
+            verifyIfHasActiveTimerAndShowCorrespondent(tabId);
         });
 
-        document.getElementById('sleepDiv').style.display = 'none';
-        document.getElementById('cancelDiv').style.display = 'unset';
+
     }, false);
+
+
+
 
     var cancelSleepButton = document.getElementById('cancelSleep');
     cancelSleepButton.addEventListener('click', function () {
-        chrome.alarms.clear(alarmName);
-        document.getElementById('sleepDiv').style.display = 'unset';
-        document.getElementById('cancelDiv').style.display = 'none';
+        chrome.tabs.getSelected(null, function (tab) {
+            let tabId = tab.id;
+            chrome.storage.local.get(tabId + '', (tabTimerInfo) => {
+                if (tabTimerInfo[tabId] != null) {
+                    chrome.alarms.clear(tabTimerInfo[tabId].alarmName);
+                    chrome.storage.local.remove(tabId + '');
+                }
+                verifyIfHasActiveTimerAndShowCorrespondent(tabId);
+            })
+        });
+
     }, 'true');
+
+
+
+    chrome.tabs.getSelected(null, function (tab) {
+        let tabId = tab.id;
+        verifyIfHasActiveTimerAndShowCorrespondent(tabId);
+    });
+
+
 }, false);
 
+function showMinutes(minutesLeft) {
+    let minutes = Math.trunc(minutesLeft);
+    let seconds = Math.trunc((minutesLeft - minutes) * 60);
+    return `${minutes}:${seconds}`;
+}
+
+function verifyIfHasActiveTimerAndShowCorrespondent(tabId) {
+    chrome.storage.local.get(tabId + '', (tabTimerInfo) => {
+        if (tabTimerInfo[tabId] != null) {
+            document.getElementById('sleepDiv').style.display = 'none';
+            document.getElementById('cancelDiv').style.display = 'unset';
+
+            var minutesLeft = (tabTimerInfo[tabId].expiresIn - new Date()) / 60000;
+            document.getElementById('timeLeft').textContent = showMinutes(minutesLeft);
+            return;
+        }
+
+        document.getElementById('sleepDiv').style.display = 'unset';
+        document.getElementById('cancelDiv').style.display = 'none';
+    });
+
+}
